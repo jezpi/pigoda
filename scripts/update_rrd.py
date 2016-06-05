ca1864e0b6fea20bbbccdcd9cbf0caa155667fc7
jezpi <jez@obin.org>
2016-06-05 21:41:01 +0200

    path change update

--- scripts/update_rrd.py
+++ scripts/update_rrd.py
@@ -5,72 +5,17 @@ import rrdtool, time,os,sys,datetime,sqlite3;
 
 def update_db_vc_temp(sqlitedb):
 	conn = sqlite3.connect(sqlitedb);
-	st = os.stat('temperature.rrd')
+	st = os.stat('/var/db/rrdcache/vc_temp.rrd')
 	print str(st.st_mtime);
 	cur = conn.cursor();
 	cur.execute("SELECT * from vc_temp_now WHERE qtime > ?", [str(st.st_mtime)]);
 	for row in cur:
 		sup=str(row[0])+":"+str(row[1]);
 		#print sup;
-		rrdtool.update('temperature.rrd', sup);
+		rrdtool.update('/var/db/rrdcache/vc_temp.rrd', sup);
 	conn.close()
 
 
-
-def update_db_tempin(sqlitedb):
-	st = os.stat('tempin.rrd');
-	conn = sqlite3.connect(sqlitedb);
-	cur = conn.cursor();
-	cur.execute("SELECT * from temp_in WHERE timestamp > ?", [str(st.st_mtime)]);
-	for row in cur:
-		sup=str(row[0])+":"+str(row[1]);
-		#print sup;
-		rrdtool.update('tempin.rrd', sup);
-	conn.close()
-
-def update_db_tempout(sqlitedb):
-	st = os.stat('tempout.rrd');
-	conn = sqlite3.connect(sqlitedb);
-	cur = conn.cursor();
-	cur.execute("SELECT * from temp_out WHERE timestamp > ?", [str(st.st_mtime)]);
-	for row in cur:
-		sup=str(row[0])+":"+str(row[1]);
-		#print sup;
-		rrdtool.update('tempout.rrd', sup);
-	conn.close()
-
-def update_db_light(sqlitedb):
-	st = os.stat('light.rrd');
-	conn = sqlite3.connect(sqlitedb);
-	cur = conn.cursor();
-	cur.execute("SELECT * from light WHERE times > ?", [str(st.st_mtime)]);
-	for row in cur:
-		sup=str(row[0])+":"+str(row[1]);
-		#print sup;
-		rrdtool.update('light.rrd', sup);
-	conn.close()
-
-def update_db_pressure(sqlitedb):
-	st = os.stat('pressure.rrd');
-	conn = sqlite3.connect(sqlitedb);
-	cur = conn.cursor();
-	cur.execute("SELECT * from pressure WHERE timestamp > ?", [str(st.st_mtime)]);
-	for row in cur:
-		sup=str(row[0])+":"+str(row[1]);
-		#print sup;
-		rrdtool.update('pressure.rrd', sup);
-	conn.close()
-
-def update_db_pir(sqlitedb):
-	st = os.stat('pir.rrd');
-	conn = sqlite3.connect(sqlitedb);
-	cur = conn.cursor();
-	cur.execute("SELECT * from pir WHERE timestamp > ?", [str(st.st_mtime)]);
-	for row in cur:
-		sup=str(row[0])+":"+str(row[1]);
-		#print sup;
-		rrdtool.update('pir.rrd', sup);
-	conn.close()
 
 
 def update_db(sqlitedb, table, rrdfile, ts='timestamp'):
@@ -85,45 +30,45 @@ def update_db(sqlitedb, table, rrdfile, ts='timestamp'):
 	conn.close()
 
 
-dbfile="/home/jez/code/MQTT/graph_channel/sensors.db";
+dbfile="/var/db/pigoda/sensors.db";
 if sys.argv[1] == "vc_temp":
 	print "=> Updating vc_temp";
 	try:
-		update_db_vc_temp('/home/jez/temperature.db')
+		update_db_vc_temp('/var/db/pigoda/vc_temp.db')
 	except:
 		print "Failed to update vc_temp"
 elif sys.argv[1] == "tempin":
 	print "=> Updating tempin";
 	try:
-		update_db(dbfile, "temp_in", "tempin.rrd");
+		update_db(dbfile, "temp_in", "/var/db/rrdcache/tempin.rrd");
 		#update_db_tempin(dbfile)
 	except:
 		print "Failed to update tempin"
 elif sys.argv[1] == "tempout":
 	print "=> Updating tempout";
 	try:
-		update_db(dbfile, "temp_out", "tempout.rrd");
+		update_db(dbfile, "temp_out", "/var/db/rrdcache/tempout.rrd");
 		#update_db_tempout(dbfile)
 	except:
 		print "failed to update tempout"
 elif sys.argv[1] == "light":
 	print "=> Updating light";
 	try:
-		update_db(dbfile, "light", "light.rrd", "times");
+		update_db(dbfile, "light", "/var/db/rrdcache/light.rrd", "times");
 		#update_db_light(dbfile)
 	except:
 		print "failed to update light"
 elif sys.argv[1] == "pressure":
 	print "=> Updating pressure";
 	try:
-		update_db(dbfile, "pressure", "pressure.rrd");
+		update_db(dbfile, "pressure", "/var/db/rrdcache/pressure.rrd");
 		#update_db_pressure(dbfile)
 	except:
 		print "failed to update pressure"
 elif sys.argv[1] == "pir":
 	print "=> Updating pir";
 	try:
-		update_db(dbfile, "pir", "pir.rrd");
+		update_db(dbfile, "pir", "/var/db/rrdcache/pir.rrd");
 		#update_db_pir(dbfile)
 	except:
 		print "failed to update pir"
