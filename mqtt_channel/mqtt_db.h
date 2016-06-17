67786d2a4069698bf82b3ecedb1d943dbc26bc16
jezjestem <jez@obin.org>
2016-06-17 14:40:07 +0200

    mq2 support

--- mqtt_channel/mqtt_db.h
+++ mqtt_channel/mqtt_db.h
@@ -2,7 +2,7 @@
 #ifndef _MQTT_DB_H_
 #define  _MQTT_DB_H_
 
-typedef enum {T_LIGHT, T_TEMPIN, T_TEMPOUT, T_PRESSURE, T_HUMIDITY, T_PIR} MQTT_data_type_t;
+typedef enum {T_LIGHT, T_TEMPIN, T_TEMPOUT, T_PRESSURE, T_HUMIDITY, T_PIR, T_MQ2} MQTT_data_type_t;
 
 typedef struct {
 	sqlite3 *Mdb_hnd;
