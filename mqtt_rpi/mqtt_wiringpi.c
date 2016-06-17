0227db479e61e2a93ebc19e7430cdf74beaf8955
jezjestem <jez@obin.org>
2016-06-17 14:35:44 +0200

    Added functionality to blink with two leds. Then the green led goes off! Red light from now will be used only to report internal problems like (the only one which is so far implemented) process termination due connection failure.

--- mqtt_rpi/mqtt_wiringpi.c
+++ mqtt_rpi/mqtt_wiringpi.c
@@ -49,20 +49,36 @@ main(int argc, char **argv)
 	return (0);
 }
 */
-
+static const unsigned short red_led_pin = 0;
+static const unsigned short green_led_pin = 2;
 
 int 
-startup_led_act() 
+startup_led_act(int ledticks) 
 {
 	int n = 0;
-	pinMode(2, OUTPUT);
-	digitalWrite(2, HIGH);
-	for (n = 0; n < 5; n++ ) {
-		digitalWrite(2, LOW);
-		delay(200);
-		digitalWrite(2, HIGH);
-		delay(200);
+	short ticktack = 1;
+	pinMode(green_led_pin, OUTPUT);
+	pinMode(red_led_pin, OUTPUT);
+	digitalWrite(green_led_pin, HIGH);
+	digitalWrite(red_led_pin, HIGH);
+	for (n = 0; n < ledticks; n++ ) {
+		if (ticktack) { 
+			digitalWrite(green_led_pin, LOW);
+			delay(200);
+			digitalWrite(green_led_pin, HIGH);
+			delay(200);
+			ticktack = 0;
+		} else {
+			digitalWrite(0, LOW);
+			delay(200);
+			digitalWrite(0, HIGH);
+			delay(200);
+			ticktack = 1;
+		}
+
 	}
+	digitalWrite(red_led_pin, LOW);
+	digitalWrite(green_led_pin, LOW);
 }
 #define FAN_PIN 1
 int startup_fanctl()
@@ -74,9 +90,27 @@ int startup_fanctl()
 
 
 int 
-term_led_act() 
+term_led_act(short failure) 
 {
-	digitalWrite(2, LOW);
+	digitalWrite(green_led_pin, LOW);
+	if (failure)
+		digitalWrite(red_led_pin, HIGH);
+}
+
+int
+flash_led(short ledid, int act) {
+	int ret = 0;
+	switch(ledid) {
+		case RED_LED:
+			digitalWrite(red_led_pin, act);
+			break;
+		case GREEN_LED:
+			digitalWrite(green_led_pin, act);
+			break;
+		default:
+			ret = -1;
+	}
+	return (ret);
 }
 
 int
