1ac8e1887455d43079c06829688e7cb2aeb006f7
jezjestem <jez@obin.org>
2016-06-17 14:36:09 +0200

    new function flash_led added

--- mqtt_rpi/mqtt_wiringpi.h
+++ mqtt_rpi/mqtt_wiringpi.h
@@ -1,11 +1,14 @@
 #ifndef _MQTT_WIRINGPI_H_
 #define  _MQTT_WIRINGPI_H_
 
+#define RED_LED 0x1
+#define GREEN_LED 0x2
 #define FAN_ON 0x1
 #define FAN_OFF 0x2
 extern int startup_fanctl();
 extern int fanctl(short, int *);
-extern int startup_led_act();
-extern int term_led_act() ;
+extern int startup_led_act(int);
+extern int term_led_act(short) ;
+extern int flash_led(short, int);
 
 #endif /*  !_MQTT_WIRINGPI_H_ */
