f74fb3f22d8a7fd0f958801a4519e0e8197271c7
jezpi <jez@obin.org>
2016-11-08 21:47:53 +0100

    cd.

--- scripts/create_rrds.sh
+++ scripts/create_rrds.sh
@@ -18,7 +18,7 @@ echo "=> Creating rrd db"
 
 
 
-createdb_tempIn () {
+createdb_tempin () {
 	rm -v ${RRDFILE}
 	printf "=> Creating rrd db: %s %d\n" "${RRDFILE}" "$offs"
 	date --date='@'"${offs}"
@@ -31,6 +31,7 @@ createdb_tempIn () {
 		RRA:AVERAGE:0.5:3600:13392 
 
 
+	sqlite3 -separator " " ${SQLITE_DB_PATH} 'select timestamp,temp from temp_in where timestamp > '$offs';'|awk '{system("rrdupdate '${RRD_DB_PATH}/'tempin.rrd "$1":"$2);printf(".");}'
 }
 
 createdb_tempout () {
@@ -44,6 +45,8 @@ createdb_tempout () {
 		RRA:AVERAGE:0.5:1:864000 \
 		RRA:AVERAGE:0.5:60:129600 \
 		RRA:AVERAGE:0.5:3600:13392 
+
+	sqlite3 -separator " " ${SQLITE_DB_PATH} 'select timestamp,temp from temp_out where timestamp > '$offs';'|awk '{system("rrdupdate '${RRD_DB_PATH}/'tempout.rrd "$1":"$2);printf(".");}'
 }
 
 createdb_pir () {
@@ -82,37 +85,58 @@ create_pressure () {
 
 }
 
+
+createdb_light () {
+        rm -v ${RRDFILE}
+        echo "=> Creating rrd db"
+        rrdtool create ${RRDFILE} \
+                --step 60 \
+                --start $offs  \
+                DS:light:GAUGE:120:0:24000 \
+                RRA:AVERAGE:0.5:1:864000 \
+                RRA:AVERAGE:0.5:60:129600 \
+                RRA:AVERAGE:0.5:3600:13392 
+
+	echo "=> Filling rrd db with data"
+	sqlite3 -separator " " ${SQLITE_DB_PATH} 'select times,val from light where times > '$offs';'|awk '{system("rrdupdate '${RRD_DB_PATH}/'light.rrd "$1":"$2);printf(".");}'
+}
+
+
+
 #############
 # main
 ###
 
 curdate=`/usr/bin/date +%s`
 offs=$((curdate-43200))
-echo "$offs"
-
-cmd=$1
-case $cmd in
-	tempout)
-		RRDFILE="/var/db/pigoda/rrd/tempout.rrd"
-		createdb_tempout;
-		;;
-	tempin)
-		RRDFILE="/var/db/pigoda/rrd/tempin.rrd"
-		createdb_tempout;
-		;;
-	light)
-		RRDFILE="/var/db/pigoda/rrd/light.rrd"
-		;;
-	pressure)
-		RRDFILE="/var/db/pigoda/rrd/pressure.rrd"
-		create_pressure;
-		;;
-	pir)
-		RRDFILE="/var/db/pigoda/rrd/pir.rrd"
-		createdb_pir;
-		;;
-	*)
-		echo "usage: $0 [tempin|tempout]"
-		exit 64
-		;;
-esac
+#echo "$offs"
+for cmd in $@
+do
+	case $cmd in
+		tempout)
+			RRDFILE="/var/db/pigoda/rrd/tempout.rrd"
+			createdb_tempout;
+			;;
+		tempin)
+			RRDFILE="/var/db/pigoda/rrd/tempin.rrd"
+			createdb_tempin;
+			;;
+		light)
+			RRDFILE="/var/db/pigoda/rrd/light.rrd"
+			createdb_light;
+			;;
+		pressure)
+			RRDFILE="/var/db/pigoda/rrd/pressure.rrd"
+			create_pressure;
+			;;
+		pir)
+			RRDFILE="/var/db/pigoda/rrd/pir.rrd"
+			createdb_pir;
+			;;
+		*)
+			echo "Invalid argument $cmd"
+			echo "usage: $0 [tempin|tempout|light|pressure|pir]"
+			exit 64
+			;;
+	esac
+done
