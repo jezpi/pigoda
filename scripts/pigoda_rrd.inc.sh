d9d0c88b96689377a42bf1ae784f026804b0fa0e
jezpi <jez@obin.org>
2016-11-08 21:26:41 +0100

    init

---
+++ scripts/pigoda_rrd.inc.sh
@@ -0,0 +1,8 @@
+
+
+# path to sqlite3 database
+: ${SQLITE_DB_PATH:="/var/db/pigoda/sensors.db"}
+: ${RRD_DB_PATH:="/var/db/pigoda/rrd"}
+: ${RRD_FILE_TEMPIN:="/var/db/pigoda/rrd/tempin.rrd"}
+: ${RRD_FILE_TEMPOUT:="/var/db/pigoda/rrd/tempout.rrd"}
+: ${RRD_GRAPH_PATH:="/var/www/pigoda"}
