08865adbc7cd45f06034a5de2ce61e7258bfee5e
Jez <jez@obin.org>
2016-11-15 10:53:56 +0100

    dynamic sensor support

--- mqtt_rpi/mqtt.h
+++ mqtt_rpi/mqtt.h
@@ -3,7 +3,26 @@
  */
 #ifndef _MQTT_H_
 #define  _MQTT_H_
+typedef enum {SENS_W1, SENS_I2C, SENS_DHTXX} stype_t;   
+typedef enum {I2C_PCF8591P, I2C_BMP85} i2ctype_t;   
 
+struct sensor {
+	char *s_name;
+	char *s_channel;
+	stype_t 	s_type;
+	i2ctype_t 	s_i2ctype;
+	char *s_config;
+	char *s_address;
+	struct sensor *s_next;
+} ;
+typedef struct sensor sensor_t ;
+
+typedef struct sensors {
+	int 		sn_count;
+	sensor_t 	*sn_head;
+	sensor_t 	*sn_tail;
+	
+} sensors_t;
 typedef struct mqtt_global_config_t {
 	const char *pidfile;
 	const char *logfile;
@@ -15,6 +34,7 @@ typedef struct mqtt_global_config_t {
 	unsigned short mqtt_port;
 	char *mqtt_channels[100];
 	unsigned int pool_sensors_delay;
+	sensors_t 	*sensors;
 } mqtt_global_cfg_t;
 
 #define DEFAULT_CONFIG_FILE	"mqtt_rpi.yaml"
