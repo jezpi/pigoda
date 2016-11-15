ad883fe95db078045742a2179b3881d5fa74faca
Jez <jez@obin.org>
2016-11-15 10:55:21 +0100

    sensor initialization. "dynamic sensor"

--- mqtt_rpi/mqtt_sensors.h
+++ mqtt_rpi/mqtt_sensors.h
@@ -6,8 +6,8 @@
 #define  _MQTT_SENSORS_H_
 
 #define PIN_BASE 220
-extern int sensors_init(void);
-extern int pcf8591p_ain(unsigned int);
+extern int sensors_init(sensors_t *);
+extern float pcf8591p_ain(char *);
 extern float get_temperature(const char *);
 extern double get_pressure(void);
 extern void bmp85_init(void);
