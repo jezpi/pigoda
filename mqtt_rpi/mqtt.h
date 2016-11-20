939c12982a2c34f93d1d6413d59379c6f50a0754
jezpi <jez@obin.org>
2016-11-20 19:23:31 +0100

    removed structures related to parser and sensor configuration from main header file

--- mqtt_rpi/mqtt.h
+++ mqtt_rpi/mqtt.h
@@ -3,58 +3,6 @@
  */
 #ifndef _MQTT_H_
 #define  _MQTT_H_
-typedef enum {SENS_W1, SENS_I2C, SENS_DHTXX} stype_t;   
-typedef enum {I2C_PCF8591P, I2C_BMP85} i2ctype_t;   
-
-typedef enum {LED_FAILURE, LED_NOTIFY} ledact_t;
-
-typedef struct led {
-	char 	*l_name;
-	int 	l_pin; 	
-	ledact_t 	l_action;
-	struct led *l_next;
-} led_t;
-
-led_t 	*curled;
-
-typedef struct ledset {
-	led_t 	*ls_head;
-	led_t 	*ls_tail;
-} ledset_t;
-
-
-struct sensor {
-	char *s_name;
-	char *s_channel;
-	stype_t 	s_type;
-	i2ctype_t 	s_i2ctype;
-	char *s_config;
-	char *s_address;
-	struct sensor *s_next;
-} ;
-typedef struct sensor sensor_t ;
-
-typedef struct sensors {
-	int 		sn_count;
-	sensor_t 	*sn_head;
-	sensor_t 	*sn_tail;
-	
-} sensors_t;
-typedef struct mqtt_global_config_t {
-	const char *pidfile;
-	const char *logfile;
-	char 		*identity;
-	short		daemon;
-	unsigned int debug_level;
-	const char *mqtt_host;
-	const char *mqtt_user;
-	const char *mqtt_password;
-	unsigned short mqtt_port;
-	unsigned int pool_sensors_delay;
-	sensors_t 	*sensors;
-	ledset_t 	*leds;
-} mqtt_global_cfg_t;
-
 
 #define DEFAULT_CONFIG_FILE	"mqtt_rpi.yaml"
 #define HOST_NAME_MAX	64
