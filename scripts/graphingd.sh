ad6a2883fd365ac7f429fda7578d9923851944ac
jez <jez@obin.org>
2016-12-02 12:14:42 +0100

    added some functionality to the daemon. Now I think I can use more MQTT protocol and concept of controlling the status of all daemons sould be implemented (since i use more than 2 daemons in MQTT network

--- scripts/graphingd.sh
+++ scripts/graphingd.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+sigint() {
+	echo "SIGINT arr"
+	mosquitto_pub -r -h localhost -u guernika -P umyka -t '/network/broadcast/mqtt_graph' -m 'off' 
+	exit 3
+}
+
+trap sigint SIGINT
+mosquitto_pub -r -h localhost -u guernika -P umyka -t '/network/broadcast/mqtt_graph' -m 'on' 
 tput clear
 while true;
 do
 	./graphing.sh exp
+	./graphing.sh daily
 	./graphing.sh everything
+	./graphing.sh custom
 	printf "Last update %s\n" "`date`"
 	sleep 300
 	tput clear
