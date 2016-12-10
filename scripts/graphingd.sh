0971263b164e7dbeaef84a9bfa8d9532a6a14ea4
jez <jez@obin.org>
2016-12-10 20:47:27 +0100

    some debug message and graphing daily stats

--- scripts/graphingd.sh
+++ scripts/graphingd.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 sigint() {
-	echo "SIGINT arr"
+	echo "mqtt_graph = off"
 	mosquitto_pub -r -h localhost -u guernika -P umyka -t '/network/broadcast/mqtt_graph' -m 'off' 
 	exit 3
 }
 
 trap sigint SIGINT
+echo "mqtt_graph = on"
 mosquitto_pub -r -h localhost -u guernika -P umyka -t '/network/broadcast/mqtt_graph' -m 'on' 
 tput clear
 while true;
 do
 	./graphing.sh exp
-	./graphing.sh daily
+	./graphing.sh everything_daily
 	./graphing.sh everything
 	./graphing.sh custom
 	printf "Last update %s\n" "`date`"
