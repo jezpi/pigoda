0be209e5c3acfd0a4e9e98ff65312d580f161ddb
jez <jez@obin.org>
2016-12-10 20:45:20 +0100

    changed default graph path to /var/www/pigoda and export of env variable from RRD_GRAPH_PATH to PNG_GRAPH_PATH

--- scripts/pigoda_rrd.inc.sh
+++ scripts/pigoda_rrd.inc.sh
@@ -5,5 +5,5 @@
 : ${RRD_DB_PATH:="/var/db/pigoda/rrd"}
 : ${RRD_FILE_TEMPIN:="/var/db/pigoda/rrd/tempin.rrd"}
 : ${RRD_FILE_TEMPOUT:="/var/db/pigoda/rrd/tempout.rrd"}
-: ${RRD_GRAPH_PATH:="/var/www/default/mq_graphs"}
-
+: ${PNG_GRAPH_PATH:="/var/www/pigoda/"}
+export PNG_GRAPH_PATH
