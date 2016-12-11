e86500eb8643eaa511af745bf54d3d7ef7f7af8b
jez <jez@obin.org>
2016-12-11 22:06:53 +0100

    changed the name of variable and exported to env to be used with make(1)

--- scripts/pigoda_rrd.inc.sh
+++ scripts/pigoda_rrd.inc.sh
@@ -6,4 +6,6 @@
 : ${RRD_FILE_TEMPIN:="/var/db/pigoda/rrd/tempin.rrd"}
 : ${RRD_FILE_TEMPOUT:="/var/db/pigoda/rrd/tempout.rrd"}
 : ${PNG_GRAPH_PATH:="/var/www/pigoda/"}
+: ${WEB_GRAPH_IMG_CLASS:="img-responsive center-block"}
 export PNG_GRAPH_PATH
+export WEB_GRAPH_IMG_CLASS
