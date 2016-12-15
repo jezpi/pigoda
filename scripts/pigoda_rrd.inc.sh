0a30da169eef93fa8b676d6df990c17f10d8e402
jez <jez@obin.org>
2016-12-15 20:09:14 +0100

    changed default db path

--- scripts/pigoda_rrd.inc.sh
+++ scripts/pigoda_rrd.inc.sh
@@ -1,7 +1,7 @@
 
 
 # path to sqlite3 database
-: ${SQLITE_DB_PATH:="/var/db/pigoda/sensors.db"}
+: ${SQLITE_DB_PATH:="/var/db/pigoda/sensorsv2.db"}
 : ${RRD_DB_PATH:="/var/db/pigoda/rrd"}
 : ${RRD_FILE_TEMPIN:="/var/db/pigoda/rrd/tempin.rrd"}
 : ${RRD_FILE_TEMPOUT:="/var/db/pigoda/rrd/tempout.rrd"}
@@ -9,3 +9,4 @@
 : ${WEB_GRAPH_IMG_CLASS:="img-responsive center-block"}
 export PNG_GRAPH_PATH
 export WEB_GRAPH_IMG_CLASS
+export SQLITE_DB_PATH
